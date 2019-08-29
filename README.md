5c9bf95e2ac4306923cb106835739cc24cc4adfc
xxsimoxx <simospa@gieffeedizioni.it>
2019-08-29 13:21:15 +0200

    added support for options

--- README.md
+++ README.md
@@ -6,15 +6,16 @@ This plugin is for fixing some compatibility issues for ClassicPress.
 * SEO by Rank Math (v. 1.0.30.2)
 
 ### Fix wp-cli 
-* Fix wp core check-update
+* Fix wp core check-update (only supports --fields and --format options)
 
 ### Notices on plugin page
 * Mark plugins not compatible with WP version 4.9 in plugins admin page
 * Add a menu under "tools" that displays top 200 plugins from wp.org and their compatibility
 
 ## Changelog
-* 2019/08/27 v. 0.0.7
+* 2019/08/29 v. 0.0.7
    * wp core check-update response is closer at the original behaviour
+   * added
 * 2019/07/29 v. 0.0.6
    * Fixed SEO by Rank Math (v. 1.0.30.2)
    * moved to "Tools" menu
--- cpcompatibility.php
+++ cpcompatibility.php
@@ -11,10 +11,12 @@
 */
 if (!defined('ABSPATH')) die('uh');
 
+ini_set('display_startup_errors', true);
+error_reporting(E_ALL);
+ini_set('display_errors', true);
+
 // Fixing plugins
 $fixed_plugins = array();
-// Slug of fixed plugins for the plugin page
-
 
 // Fix rankmath
 add_action( 'admin_init', 'cpcompatibility_fix_rankmath' );
@@ -42,12 +44,26 @@ if ( function_exists( 'classicpress_version' ) && defined( 'WP_CLI' ) && WP_CLI 
 
 function cp_correct_check_update( ) {
 	// if we have ClassicPress
-	if ( function_exists( 'classicpress_version' ) ) {
+	if ( function_exists( 'classicpress_version' ) && WP_CLI ) {
 		$gcu =  get_core_updates();
 		if ( 'latest' == $gcu[0]->{'response'} ){ 
 			WP_CLI::success( "ClassicPress is at the latest version." );
 		} else {
 			if ( $gcu ) {
+				// Retrieve options in a very unusual way
+				$cp_current_pid = getmypid();
+				$cp_current_command = strtr ( trim(file_get_contents("/proc/$cp_current_pid/cmdline"), "\0"), "\0", " ");
+				$cp_fields_match = array();
+				if ( preg_match_all ( '/ --fields=([a-z,_]+)/' , $cp_current_command, $cp_fields_match ) > 0 ){
+					$cp_arg_fields = $cp_fields_match[1][0] ;
+				} else {
+					$cp_arg_fields = "version,update_type,package_url";
+				}
+				if ( preg_match_all ( '/ --format=([a-z]+)/' , $cp_current_command, $cp_format_match ) > 0 ){
+					$cp_format_fields = $cp_format_match[1][0];
+				} else {
+					$cp_format_fields = "table";
+				}
 				// $cp_version is only needed to evalutate "update_type"
 				// Is the right way to find the path?
 				include( get_home_path() . "/wp-includes/version.php" );
@@ -55,7 +71,7 @@ function cp_correct_check_update( ) {
 				$cp_table_output[0]["package_url"] = $gcu[0]->{'download'};
 				// Don't break anything if $cp_version is null
 				$cp_table_output[0]["update_type"] = ( isset ( $cp_version ) ? WP_CLI\Utils\get_named_sem_ver( $gcu[0]->{'version'}, $cp_version ) : "" );
-				WP_CLI\Utils\format_items( 'table', $cp_table_output, array( 'version', 'update_type', 'package_url' ) );
+				WP_CLI\Utils\format_items( $cp_format_fields, $cp_table_output, $cp_arg_fields );
 			}
 		};
 		// then exit to prevent the core check-update command to 
