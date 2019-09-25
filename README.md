3bb5f80caf1512a19264a5e8c2966448be6ad329
xxsimoxx <simospa@gieffeedizioni.it>
2019-09-25 09:38:51 +0200

    updates check

--- README.md
+++ README.md
@@ -13,19 +13,27 @@ This plugin is for fixing some compatibility issues for ClassicPress.
 * Mark plugins not compatible with WP version 4.9 in plugins admin page
 * Add a menu under "tools" that displays top 200 plugins from wp.org and their compatibility
 
+### Updating
+This plugin supports [GitHub Updater](https://github.com/afragen/github-updater).
+This allows to upgrade to the latest code.
+If not installed, you will be noticed about new releases on the plugin page,
+and you'll need to install the new version manually.
+
 ## Changelog
-* 2019/09/20 v0.1.0
+* 2019/09/25 v0.0.10
 	* added `wp plugin latestgit` to install from GitHub
 	* function `cpcompatibility_fixed_plugin()` return an array of slugs of fixed plugins
 	* wp core check-update now supports --fields and --format options
-* 2019/09/05 v.0.0.9
+	* support for GitHub Updater (https://github.com/afragen/github-updater)
+	* self-checking for new releases
+* 2019/09/05 v0.0.9
    * split file/directory structure
    * better CSS
    * i18n 
-* 2019/09/02 v.0.0.8
+* 2019/09/02 v0.0.8
    * bugfix
-* 2019/08/29 v.0.0.7
+* 2019/08/29 v0.0.7
    * wp core check-update response is closer at the original behaviour
-* 2019/07/29 v.0.0.6
+* 2019/07/29 v0.0.6
    * Fixed SEO by Rank Math (v. 1.0.30.2)
    * moved to "Tools" menu
--- cpcompatibility.php
+++ cpcompatibility.php
@@ -1,15 +1,16 @@
 <?php
 /*
-* Plugin Name: CPcompatibility
-* Plugin URI: https://www.gieffeedizioni.it/classicpress
-* Description: Tweaks for working with CP: wpcli compatibility, plugin checks. 
-* Version: 0.0.10
-* License: GPL2
-* License URI: https://www.gnu.org/licenses/gpl-2.0.html
-* Author: Gieffe edizioni srl
-* Author URI: https://www.gieffeedizioni.it/classicpress
-* Text Domain: cpc
-*/
+ * Plugin Name: CPcompatibility
+ * Plugin URI: https://github.com/xxsimoxx/cpcompatibility
+ * Description: Tweaks for working with CP: wpcli compatibility, plugin checks. 
+ * Version: 0.0.10
+ * License: GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Author: Gieffe edizioni srl
+ * Author URI: https://www.gieffeedizioni.it/classicpress
+ * Text Domain: cpc
+ * GitHub Plugin URI: xxsimoxx/cpcompatibility
+ */
 
 if (!defined('ABSPATH')) die('uh');
 
@@ -54,4 +55,47 @@ add_action( 'plugins_loaded', 'cpc_load_textdomain' );
 function cpc_load_textdomain() {
 	load_plugin_textdomain( 'cpc', false, basename( dirname( __FILE__ ) ) . '/languages' ); 
 }
+
+// check for updates
+function cpc_update_link(){
+	if ( is_plugin_active("github-updater/github-updater.php") ){
+		return false;
+		// let's github-updater handle this for us!
+	};
+	$slug = dirname( plugin_basename( __FILE__ ) );
+	$plugin_info = get_plugin_data(__FILE__);
+	$plugin_installed_version = $plugin_info['Version'];
+	$git_repo = "xxsimoxx/" . $slug;
+	if ( false === ( $plugin_current_version = get_transient( $slug . 'lastversion' ) ) ) {
+		$response = wp_remote_get( 'https://api.github.com/repos/' . $git_repo . '/releases/latest' , array( 'redirection' => 5 ) );
+		if ( 200 === $response['response']['code'] ){
+			$git_data = json_decode ( $response['body'], true );
+			$plugin_current_version = ltrim ( $git_data['tag_name'], 'v');
+		} else
+		{
+			$plugin_current_version = null;
+		};
+		if ( ! is_null( $plugin_current_version ) ) {
+			set_transient( $slug . 'lastversion', $plugin_current_version, DAY_IN_SECONDS );
+		};
+	};
+	if ( version_compare( $plugin_current_version, $plugin_installed_version , '>' ) ){
+		/*Translators: %s is the new version available */
+		$messagestring =  sprintf( __( "NEW v%s", "cpc" ), $plugin_current_version );
+		return '<a target="_blank" href="https://www.github.com/' . $git_repo . '/releases/latest">' . $messagestring . '</a>';
+	} else {
+		return false;
+	}
+};
+
+add_filter( 'plugin_action_links_' . plugin_basename(__FILE__), 'cpc_update' );
+function cpc_update( $links ) {
+	$update_link = cpc_update_link();
+	if ( $update_link ){
+		array_push( $links, $update_link );
+	};
+	return $links;
+}
 ?>
+
+
