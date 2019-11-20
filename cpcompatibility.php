201584681adfa55174e9778bdb5eab7c191961ea
xxsimoxx <simospa@gieffeedizioni.it>
2019-11-20 13:27:30 +0100

    removed GitHub updater

--- cpcompatibility.php
+++ cpcompatibility.php
@@ -9,7 +9,6 @@
  * Author: Gieffe edizioni srl
  * Author URI: https://www.gieffeedizioni.it/classicpress
  * Text Domain: cpc
- * GitHub Plugin URI: xxsimoxx/cpcompatibility
  */
 
 if (!defined('ABSPATH')) die('uh');
@@ -64,44 +63,4 @@ function cpc_load_textdomain() {
 	load_plugin_textdomain( 'cpc', false, basename( dirname( __FILE__ ) ) . '/languages' ); 
 }
 
-// check for updates
-function cpc_update_link(){
-	if ( is_plugin_active("github-updater/github-updater.php") ){
-		return false;
-		// let's github-updater handle this for us!
-	};
-	$slug = dirname( plugin_basename( __FILE__ ) );
-	$plugin_info = get_plugin_data(__FILE__);
-	$plugin_installed_version = $plugin_info['Version'];
-	$git_repo = "xxsimoxx/" . $slug;
-	if ( false === ( $plugin_current_version = get_transient( $slug . 'lastversion' ) ) ) {
-		$response = wp_remote_get( 'https://api.github.com/repos/' . $git_repo . '/releases/latest' , array( 'redirection' => 5 ) );
-		if ( 200 === $response['response']['code'] ){
-			$git_data = json_decode ( $response['body'], true );
-			$plugin_current_version = ltrim ( $git_data['tag_name'], 'v');
-		} else
-		{
-			$plugin_current_version = null;
-		};
-		if ( ! is_null( $plugin_current_version ) ) {
-			set_transient( $slug . 'lastversion', $plugin_current_version, DAY_IN_SECONDS );
-		};
-	};
-	if ( version_compare( $plugin_current_version, $plugin_installed_version , '>' ) ){
-		/*Translators: %s is the new version available */
-		$messagestring =  sprintf( __( "NEW v%s", "cpc" ), $plugin_current_version );
-		return '<a target="_blank" href="https://www.github.com/' . $git_repo . '/releases/latest">' . $messagestring . '</a>';
-	} else {
-		return false;
-	}
-};
-
-add_filter( 'plugin_action_links_' . plugin_basename(__FILE__), 'cpc_update' );
-function cpc_update( $links ) {
-	$update_link = cpc_update_link();
-	if ( $update_link ){
-		array_push( $links, $update_link );
-	};
-	return $links;
-}
 ?>
--- readme.txt
+++ readme.txt
@@ -29,33 +29,39 @@ This plugin is for fixing some compatibility issues for ClassicPress.
 * Mark plugins not compatible with WP version 4.9 in plugins admin page
 * Add a menu under "tools" that displays top 200 plugins from wp.org and their compatibility
 
-
 == Screenshots ==
 
 1. Plugin Page
 2. Most popular plugins and their compatibility
 
 == Changelog ==
+
 = 0.1.0 =
 * Add autoupdate code
-* code cleanup
+* removed GitHub updater
+
 = 0.0.12 =
 * bugfix: PHP 7.4 compatibility
+
 = 0.0.11 =
 * bugfix: wp core check-update returned an extra line
 * adds Caldera Forms > 1.8.4 (missing wp-components style)
+
 = 0.0.10 =
 * added wp plugin latestgit to install from GitHub
 * function cpcompatibility_fixed_plugin() return an array of slugs of fixed plugins
 * wp core check-update now supports --fields and --format options
 * support for GitHub Updater (https://github.com/afragen/github-updater)
 * self-checking for new releases
+
 = 0.0.9 =
 * split file/directory structure
 * better CSS
 * i18n l10n
+
 = v0.0.7 =
 * wp core check-update response is closer at the original behaviour
+
 = v0.0.6 =
 * Fixed SEO by Rank Math (v. 1.0.30.2)
 * moved to "Tools" menu
