e24ce8fcab2b873a0393c02ac48acbb19e2b2c4b
xxsimoxx <simospa@gieffeedizioni.it>
2020-04-16 15:16:12 +0200

    speed up plugins page loading

--- inc/plugin-info.php
+++ inc/plugin-info.php
@@ -1,21 +1,22 @@
 <?php
-if (!defined('ABSPATH')) die('uh');
+if (!defined('ABSPATH')) {
+	die('uh');
+}
 
 /**
  * CPplugin_info( $plugin_name ) : array( $requires, $version )
  *
  */
 function CPplugin_info($plugin_name) {
-	if (($retval = get_transient('cpc_plugin_info_' . $plugin_name)) === false) {
-		include_once (ABSPATH . "wp-admin/includes/plugin-install.php");
+	if (($retval = get_transient('cpc_plugin_info_'.$plugin_name)) === false) {
+		include_once (ABSPATH.'wp-admin/includes/plugin-install.php');
 		$queryfor = ['slug' => $plugin_name];
-		$plugin_info = plugins_api('plugin_information', $queryfor);
 		if (!is_wp_error($plugin_info = plugins_api('plugin_information', $queryfor))) {
 			$retval = [$plugin_info->requires, $plugin_info->version];
 			set_transient('cpc_plugin_info_'.$plugin_name, $retval, 24 * HOUR_IN_SECONDS);
 		} else {
-			set_transient('cpc_plugin_info_'.$plugin_name, "", 24 * HOUR_IN_SECONDS);
-		return "";
+			set_transient('cpc_plugin_info_'.$plugin_name, '', 24 * HOUR_IN_SECONDS);
+		return '';
 		}
 	}
 	return $retval;
