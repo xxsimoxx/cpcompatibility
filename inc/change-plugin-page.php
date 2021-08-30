1a6c6a0d288412f753b2ebb597ac9c8ef238613e
xxsimoxx <simospa@gieffeedizioni.it>
2021-08-30 14:33:19 +0200

    Speed up

--- inc/change-plugin-page.php
+++ inc/change-plugin-page.php
@@ -3,68 +3,90 @@ if (!defined('ABSPATH')) {
 	die('uh');
 }
 
-function cp_plugin_info() {
+class CPCompatibilityNotice {
 
-	// Check if we already have a transient for that
-	$plugin_info = get_transient('cpc_plugin_info');
-	if ($plugin_info !== false) {
-		return $plugin_info;
+	private $plugin_info_cache = false;
+
+	public function __construct() {
+		add_filter('plugin_row_meta', [$this, 'plugin_row_meta'], 10, 2);
 	}
 
-	// Get an array of all installed plugins.
-	$all_plugins = get_plugins();
-	foreach ($all_plugins as $path => $value) {
-		$slugs[] = dirname($path);
-	}
+	private function get_plugin_info() {
 
-	// Run a query to get informations
-	include_once (ABSPATH.'wp-admin/includes/plugin-install.php');
-	$queryfor = ['slugs' => $slugs];
-	if (is_wp_error($plugin_info = plugins_api('plugin_information', $queryfor))) {
-		return false;
-	}
+		// Check if we already have that in memory
+		if ($this->plugin_info_cache !== false) {
+			return $this->plugin_info_cache;
+		}
 
-	foreach ($plugin_info as $slug => $info) {
-		foreach ($info as $property_name => $property_value) {
-			if (in_array($property_name, ['version', 'requires'])) {
+		// Check if we already have a transient for that
+		$plugin_info = get_transient('cpc_plugin_info');
+		if ($plugin_info !== false) {
+			return $plugin_info;
+		}
+
+		// Get an array of all installed plugins.
+		$all_plugins = get_plugins();
+		foreach ($all_plugins as $path => $value) {
+			$slugs[] = dirname($path);
+		}
+
+		// Run a query to get informations
+		include_once (ABSPATH.'wp-admin/includes/plugin-install.php');
+		$query = ['slugs'  => $slugs];
+		if (is_wp_error($plugin_info = plugins_api('plugin_information', $query))) {
+			return false;
+		}
+
+		// Strip down unnecessary data
+		foreach ($plugin_info as $slug => $info) {
+			foreach ($info as $property_name => $property_value) {
+				if (in_array($property_name, ['version', 'requires'])) {
+					continue;
+				}
+				unset($plugin_info->{$slug}[$property_name]);
+			}
+			if (!empty($plugin_info->{$slug})) {
 				continue;
 			}
-			unset($plugin_info->{$slug}[$property_name]);
+			unset($plugin_info->{$slug});
 		}
+
+		// Save transient...
+		set_transient('cpc_plugin_info', $plugin_info, 6 * HOUR_IN_SECONDS);
+		// and keep in memory.
+		$this->plugin_info_cache = $plugin_info;
+
+		return $plugin_info;
+
 	}
 
-	set_transient('cpc_plugin_info', $plugin_info, 6 * HOUR_IN_SECONDS);
-	return $plugin_info;
+	public function plugin_row_meta($links, $file) {
+
+		$slug = dirname($file);
+
+		$plugin_info = $this->get_plugin_info();
+
+		// Check if we have data we need for the plugin
+		if (!isset($plugin_info->{$slug}['version'])) {
+			return $links;
+		}
+
+		// Ok, it don't require WP 5+
+		if (version_compare($plugin_info->{$slug}['requires'], '5', '<')) {
+			return $links;
+		}
+
+		// Check if a plugin is bumped to 999 or alike.
+		$plugindata = get_plugin_data(WP_PLUGIN_DIR.'/'.$file, false, false);
+		if (version_compare($plugindata['Version'], $plugin_info->{$slug}['version'], '>')) {
+			return $links;
+		}
+
+		// Add the notice
+		array_push($links, '<span class="dashicons-before dashicons-warning">'.sprintf (__('Requires WordPress %1$s for version %2$s.<br>', 'cpc'), $plugin_info->{$slug}['requires'], $plugin_info->{$slug}['version']).'</span>');
+		return $links;
+	}
 
 }
 
-add_filter('plugin_row_meta', 'cp_plugin_row_meta', 10, 2);
-
-function cp_plugin_row_meta($links, $file) {
-
-	$slug = dirname($file);
-	$plugin_info = cp_plugin_info();
-
-	if ($plugin_info === false) {
-		return $links;
-	}
-
-	if (!isset($plugin_info->{$slug}['version'])) {
-		return $links;
-	}
-
-	if (version_compare($plugin_info->{$slug}['requires'], '5', '<')) {
-		return $links;
-	}
-
-	// Check if a plugin is bumped to 999 or alike.
-	$plugindata = get_plugin_data(WP_PLUGIN_DIR.'/'.$file, false, false);
-	if (version_compare($plugindata['Version'], $plugin_info->{$slug}['version'], '>')) {
-		return $links;
-	}
-
-	array_push($links, '<span class="dashicons-before dashicons-warning">'.sprintf (__('Requires WordPress %1$s for version %2$s.<br>', 'cpc'), $plugin_info->{$slug}['requires'], $plugin_info->{$slug}['version']).'</span>');
-	return $links;
-}
-
-?>
+new CPCompatibilityNotice();
--- readme.txt
+++ readme.txt
@@ -1,7 +1,7 @@
 === cpcompatibility ===
 Plugin Name:        CPcompatibility
 Description:        Tweaks for working with CP: wpcli compatibility, plugin checks.
-Version:            0.7.1
+Version:            0.7.2
 Text Domain:        cpc
 Domain Path:        /languages
 Requires PHP:       5.6
@@ -10,7 +10,7 @@ Tested:             4.9.99
 Author:             Gieffe edizioni
 Author URI:         https://www.gieffeedizioni.it
 Plugin URI:         https://software.gieffeedizioni.it
-Download link:      https://github.com/xxsimoxx/cpcompatibility/releases/download/v0.7.1/cpcompatibility-0.7.1.zip
+Download link:      https://github.com/xxsimoxx/cpcompatibility/releases/download/v0.7.2/cpcompatibility-0.7.2.zip
 License:            GPLv2
 License URI:        https://www.gnu.org/licenses/gpl-2.0.html
 
@@ -31,6 +31,9 @@ To help us know the number of active installations of this plugin, we collect an
 2. Most popular plugins and their compatibility
 
 == Changelog ==
+= 0.7.2 =
+* Speed up plugin notices.
+
 = 0.7.1 =
 * Removed unused lodash lib.
 
